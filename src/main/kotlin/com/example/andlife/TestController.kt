9efaaa65057459474cfd96ee7e7c0b130f620136
Donghyun <pdchoirian@gmail.com>
2025-12-28 02:56:48 +0900

    test: RestController로 변경 후 재시도

--- src/main/kotlin/com/example/andlife/TestController.kt
+++ src/main/kotlin/com/example/andlife/TestController.kt
@@ -2,17 +2,15 @@ package com.example.andlife
 
 import com.example.andlife.entity.Member
 import com.example.andlife.service.MemberService
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class TestController(
     private val memberService: MemberService
 ) {
 
     @GetMapping("/")
-    @ResponseBody
     fun test(): String {
         return "드디어 성공했당"
     }
