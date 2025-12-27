405902eae8e33b5d58973762b96bbbeaa178b207
Donghyun <pdchoirian@gmail.com>
2025-12-27 19:08:52 +0900

    init: initial project

---
+++ src/main/kotlin/com/example/andlife/AndlifeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.andlife
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AndlifeApplication
+
+fun main(args: Array<String>) {
+	runApplication<AndlifeApplication>(*args)
+}
